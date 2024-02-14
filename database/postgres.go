eda8ea8a7e94c3935957445d900c6b7db08e91f0
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-14 14:04:27 +0300

    chore: add basic db scaffold - postgres

--- database/postgres.go
+++ database/postgres.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"blog-api/routes"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var pgPass = os.Getenv("PG_PASS")
+
+var pgConnString = "postgres://postgres.xlvhpbcxavkjwhvvumjr:" + pgPass + "@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
+
+func Database() {
+	db, err := gorm.Open(postgres.Open(pgConnString), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database" + err.Error())
+	}
+
+	db.AutoMigrate(&routes.Blog{})
+
+}
