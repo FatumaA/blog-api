3b930f34303a9d2fe2e06c4fdd1ab24f63f9fbfc
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-14 13:59:01 +0300

    feat: set up port and routes

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"blog-api/routes"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	blog := &routes.Blog{}
+
+	router := gin.Default()
+	router.GET("/blogs", blog.GetBlogs)
+	router.GET("/blogs/:id", blog.GetBlog)
+	router.POST("/blogs", blog.CreateBlog) // Convert blog.CreateBlog to gin.HandlerFunc
+	router.PUT("/blogs/:id", blog.UpdateBlog)
+	router.DELETE("/blogs/:id", blog.DeleteBlog)
+
+	router.Run("localhost:8000")
 }
